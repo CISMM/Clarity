6fc7961498e45c7d675ac56425ecc9361e264783
Cory Quammen <cquammen@cs.unc.edu>
2007-09-18 04:48:12 +0000

    2007-09-18 Cory Quammen <cquammen@cs.unc.edu>
       * WienerDeconvolve.cxx: Fixed loop index error.

--- src/WienerDeconvolve.cxx
+++ src/WienerDeconvolve.cxx
@@ -7,7 +7,7 @@ Clarity_WienerDeconvolve(float* outImage, float* inImage, float* psfImage, int d
    // Temporary code to produce something for checking whether VTK hookup works.
    int size = dimensions[0] * dimensions[1] * dimensions[2];
    for (int i = 0; i < size; i++) {
-      outImage[0] = 0.0f;
+      outImage[i] = 0.0f;
    }
 
    return CLARITY_SUCCESS;
