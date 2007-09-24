eda3fc8ee806f591a2bfe80f72c95052af3884d0
Cory Quammen <cquammen@cs.unc.edu>
2007-09-24 17:09:54 +0000

    2007-09-24 Cory Quammen <cquammen@cs.unc.edu>
       * FFT.h: Added some memory allocation functions specifically for holding
       the results of Fourier transforms (real-to-complex and complex-to-real).

--- include/FFT.h
+++ include/FFT.h
@@ -3,11 +3,30 @@
 
 #include "Clarity.h"
 
+/** Allocates memory for the complex result of a real-to-complex 
+* Fourier transform. */
+ClarityResult_t
+Clarity_R2C_Malloc(void** buffer, size_t size, int nx, int ny, int nz);
+
+/** Allocates memory for the real result of a complex-to-real
+* Fourier transform. */
+ClarityResult_t
+Clarity_C2R_Malloc(void** buffer, size_t size, int nx, int ny, int nz);
+
+void
+Clarity_Free(void* buffer);
+
 /** 3D foward FFT function. */
-ClarityResult_t fftf_r2c_3d(int nx, int ny, int nz, float* in, float* out);
+ClarityResult_t
+Clarity_FFT_R2C_3D_float(int nx, int ny, int nz, float* in, float* out);
 
 /** 3D inverse FFT function. */
-ClarityResult_t fftf_c2r_3d(int nx, int ny, int nz, float* in, float* out);
+ClarityResult_t
+Clarity_FFT_C2R_3D_float(int nx, int ny, int nz, float* in, float* out);
 
+/** Convolution function with pre-Fourier-transformed kernel. */
+ClarityResult_t
+Clarity_Convolve_OTF(int nx, int ny, int nz, float* in,
+                 float* otf, float* out);
 
 #endif // __FFT_H_
